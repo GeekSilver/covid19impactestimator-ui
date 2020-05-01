fde7e7e7a9ffe508bbe10c60dc86482d7ad20a46
GeekSilver <erickioko995@gmail.com>
2020-05-01 11:52:54 +0300

    added a table output for sm-devices and fixed graph for lg-devices

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useReducer }  from 'react';
+import React, { useReducer } from 'react';
+// eslint-disable-next-line
 import Chart from '../node_modules/chart.js/dist/Chart.bundle';
 import './bootstrap.min.css';
 import './index.css';
 
 import { useCovidEstimator } from './customHooks';
-import {Bar} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import { useState } from 'react';
 
 
@@ -12,36 +13,80 @@ function App() {
   const covidEstimatorReducer = (state, action) => {
     switch (action.type) {
       case 'ESTIMATE':
-        return {...action.data};
+        return { ...action.data };
       case 'ESTIMATING':
-        return {...state, estimating: action.estimating}
-    
+        return { ...state, estimating: action.estimating }
+      case 'ADD_VALUES':
+        return { ...state, ...action.data }
+
       default:
         return state;
     }
   }
 
-  const [data, dispatchPostData] = useReducer(covidEstimatorReducer, { fetching: false, data: {} });
+  const [data, dispatchPostData] = useReducer(covidEstimatorReducer,
+    {
+      fetching: false,
+      reportedCases: '',
+      population: '',
+      periodType: '',
+      timeToElapse: '',
+      region: '',
+      totalHospitalBeds: '',
+      avgDailyIncomePopulation: '',
+      avgDailyIncomeInUSD: ''
+    });
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // input values
+    const reportedCases = document.getElementById('reportedCases').value;
+    const population = document.getElementById('population').value;
+    const periodType = document.getElementById('periodType').value;
+    const timeToElapse = document.getElementById('timeToElapse').value;
+    const region = document.getElementById('region').value;
+    const totalHospitalBeds = document.getElementById('totalHospitalBeds').value;
+    const avgDailyIncomePopulation = document.getElementById('avgDailyIncomePopulation').value;
+    const avgDailyIncomeInUSD = document.getElementById('avgDailyIncomeInUSD').value;
+    // input values arrays
+    const validationArray = [reportedCases, population, periodType, timeToElapse, region, totalHospitalBeds, avgDailyIncomePopulation,
+      avgDailyIncomeInUSD];
+
+    if (validationArray.includes('')) {
+      document.getElementById('validationDiv').classList.remove('d-none');
+      return;
+    }
+    document.getElementById('validationDiv').classList.add('d-none');
     // set data values
-    data.reportedCases = document.getElementById('reportedCases').value;
-    data.population = document.getElementById('population').value;
-    data.periodType = document.getElementById('periodType').value;
-    data.timeToElapse = document.getElementById('timeToElapse').value;
-    data.region = document.getElementById('region').value;
-    data.totalHospitalBeds = document.getElementById('totalHospitalBeds').value;
-    data.avgDailyIncomePopulation = document.getElementById('avgDailyIncomePopulation').value;
-    data.avgDailyIncomeInUSD = document.getElementById('avgDailyIncomeInUSD').value;
-    // make chart visible
-    document.getElementById('outputChart').style.display = 'block';
-    document.getElementById('outputChart').focus();
-    return data;
+    dispatchPostData({
+      type: 'ADD_VALUES', data: {
+        reportedCases,
+        population,
+        periodType,
+        timeToElapse,
+        totalHospitalBeds,
+        region: {
+          name: region,
+          avgDailyIncomePopulation: (avgDailyIncomePopulation / 100),
+          avgDailyIncomeInUSD
+        }
+      }
+    })
+
   }
 
   // chart props
-  const [chartData, setChartData] = useState({})
+  const [chartData, setChartData] = useState({
+
+    datasets: [
+      {
+        data: []
+      }
+      , {
+        data: []
+      }]
+
+  })
   const [chartOptions, setChartOptions] = useState({});
   // using covid estimator
   useCovidEstimator(data, dispatchPostData, setChartData, setChartOptions);
@@ -52,28 +97,79 @@ function App() {
       <div className="row d-flex m-4 justify-content-center">
         <h2 className="text-center"> COVID19 ESTIMATOR </h2>
       </div>
-      <div className="row" id="outputChart" tabIndex="0" style={{display: "none"}}>
+      <div className="row d-none" id="outputChart" tabIndex="0">
         <Bar
-        data={chartData}
-        options={chartOptions}
+          data={chartData}
+          options={chartOptions}
         />
       </div>
-      <form onSubmit={handleSubmit}>
-        <h2 className="font-weight-semi-bold text-center m-2">estimate</h2>
+      <div className="d-none d-md-none bg-light" id="sm-output" tabIndex="0">
+        <table className="table table-responsive table-striped">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Impact</th>
+              <th>Severe Impact</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Currently Infected</td>
+              <td>{chartData.datasets[0].data[0]}</td>
+              <td>{chartData.datasets[1].data[0]}</td>
+            </tr>
+            <tr>
+              <td>Infections By Requested Time</td>
+              <td>{chartData.datasets[0].data[1]}</td>
+              <td>{chartData.datasets[1].data[1]}</td>
+            </tr>
+            <tr>
+              <td>Severe Cases By Requested Time</td>
+              <td>{chartData.datasets[0].data[2]}</td>
+              <td>{chartData.datasets[1].data[2]}</td>
+            </tr>
+            <tr>
+              <td>Hospital Beds By Requested Time</td>
+              <td>{chartData.datasets[0].data[3]}</td>
+              <td>{chartData.datasets[1].data[3]}</td>
+            </tr>
+            <tr>
+              <td>Cases For ICU By Requested Time</td>
+              <td>{chartData.datasets[0].data[4]}</td>
+              <td>{chartData.datasets[1].data[4]}</td>
+            </tr>
+            <tr>
+              <td>Cases For Ventilators By Requested Time</td>
+              <td>{chartData.datasets[0].data[5]}</td>
+              <td>{chartData.datasets[1].data[5]}</td>
+            </tr>
+            <tr>
+              <td>Dollars In Flight</td>
+              <td>{chartData.datasets[0].data[6]}</td>
+              <td>{chartData.datasets[1].data[6]}</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+      <form onSubmit={handleSubmit} noValidate>
+        <div className="d-none text-center" id="validationDiv">
+          <label className="text-danger">Fill all form input fields!!!</label>
+        </div>
         <div className="form-group row">
           <div className="col-6">
             <label htmlFor="reportedCases">Reported Cases</label>
-            <input type="number" id="reportedCases" data-reported-cases="" className="form-control"></input>
+            <input type="number" id="reportedCases" data-reported-cases="" className="form-control" required></input>
           </div>
           <div className="col-6">
             <label htmlFor="population">Population Size</label>
-            <input type="number" data-population="" id="population" className="form-control"></input>
+            <input type="number" data-population="" id="population" className="form-control" required></input>
           </div>
         </div>
         <div className="form-group row">
           <div className="col-6">
             <label htmlFor="periodType">Period Type</label>
-            <select id="periodType" data-period-type="" className="form-control">
+            <select id="periodType" data-period-type="" className="form-control" required>
               <option value="days" name="days">days</option>
               <option value="weeks" name="weeks">weeks</option>
               <option value="months" name="months">months</option>
@@ -81,32 +177,35 @@ function App() {
           </div>
           <div className="col-6">
             <label htmlFor="timeToElapse">Time to Elapse</label>
-            <input type="number" id="timeToElapse" data-time-to-elapse="" className="form-control"></input>
+            <input type="number" id="timeToElapse" data-time-to-elapse="" className="form-control" required></input>
           </div>
         </div>
         <div className="form-group row">
           <div className="col-4">
             <label htmlFor="region">Region</label>
-            <input id="region" type="text" className="form-control"></input>            
+            <input id="region" type="text" className="form-control" required></input>
           </div>
           <div className="col-8">
             <label htmlFor="totalHospitalBeds">Total Hospital Beds</label>
-            <input type="number" id="totalHospitalBeds" data-total-hospital-beds="" className="form-control"></input>            
+            <input type="number" id="totalHospitalBeds" data-total-hospital-beds="" className="form-control" required></input>
           </div>
         </div>
         <div className="form-group row">
           <div className="col-10 offset-1">
             <label htmlFor="avgDailyIncomePopulation">Average Daily Income Population</label>
             <div className="input-group">
-            <input type="number" id="avgDailyIncomePopulation" className="form-control"></input>
+              <input type="number" max={100} id="avgDailyIncomePopulation" className="form-control" required></input>
               <div className="input-group-append">
                 <div className="input-group-text">%</div>
+              </div>
+              <div className="invalid-feedback">
+                A percentage cannot be greater than 100 or less than 0.
               </div>
             </div>
           </div>
         </div>
         <div className="form-group row">
-        <div className="col-10 offset-1">
+          <div className="col-10 offset-1">
             <label htmlFor="avgDailyIncomeInUSD">Average Daily Income In USD</label>
             <div className="input-group">
               <div className="input-group-prepend">
@@ -114,7 +213,7 @@ function App() {
                   $
                 </div>
               </div>
-              <input type="number" step="any" min="0.00" id="avgDailyIncomeInUSD" data-avg-daily-income-in-usd="" className="form-control"></input>
+              <input type="number" step="any" min="0.00" id="avgDailyIncomeInUSD" data-avg-daily-income-in-usd="" className="form-control" required></input>
             </div>
           </div>
         </div>
--- src/customHooks.js
+++ src/customHooks.js
@@ -1,83 +1,94 @@
 import { useEffect } from "react";
 
-export const useCovidEstimator = (data, dispatch, setChartData, setChartOptions) => {
+export const useCovidEstimator = (inputData, dispatch, setChartData, setChartOptions) => {
   useEffect(() => {
-    // set data.fetching to true
-    dispatch({type: 'FETCHING', fetching: true});
-    // post data to get estimates from api
+    // set data.fetching to true 
+    dispatch({ type: 'FETCHING', fetching: true });
+    // post data to get estimates from api 
     fetch(`https://covid19impactestimator.herokuapp.com/api/v1/on-covid-19/json`,
-    {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify()
-    })
-    .then(data => data.json())
-    .then(data => {
-      // set data
-      dispatch({type: 'FETCH', data});
-      // set chartData
-      setChartData(() => {
-        return {
-          labels: [
-            'currently infected',
-            'infections by requested time',
-            'severe cases by requested time',
-            'hospital beds by requested time',
-            'cases for ICU by requested time',
-            'cases for ventilators by requested time',
-            'dollars in flight'
-          ],
-          datasets: [{
-              label: '# of Votes',
+      {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(inputData)
+      })
+      .then(data => data.json())
+      .then(data => {
+        // set data
+        dispatch({ type: 'FETCH', data });
+        // set chartData
+        setChartData(() => {
+          return {
+            labels: [
+              'currently infected',
+              'infections by requested time',
+              'severe cases by requested time',
+              'hospital beds by requested time',
+              'cases for ICU by requested time',
+              'cases for ventilators by requested time',
+              'dollars in flight'
+            ],
+            datasets: [{
+              label: 'Estimated Impact',
               data: [
-                data.impact.currentlyInfected,
-                data.impact.infectionsByRequestedTime,
-                data.impact.severeCasesByRequestedtime,
-                data.impact.hospitalBedsByRequestedTime,
-                data.impact.casesForICUByRequestedTime,
-                data.impact.casesForVentilatorsByRequestedTime,
-                data.impact.dollarsInFlight
+                Math.trunc(data.impact.currentlyInfected),
+                Math.trunc(data.impact.infectionsByRequestedTime),
+                Math.trunc(data.impact.severeCasesByRequestedTime),
+                Math.trunc(data.impact.hospitalBedsByRequestedTime),
+                Math.trunc(data.impact.casesForICUByRequestedTime),
+                Math.trunc(data.impact.casesForVentilatorsByRequestedTime),
+                Math.trunc(data.impact.dollarsInFlight)
               ],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
+              backgroundColor: '#17a2b8',
+              borderColor: '#007bff',
               borderWidth: 1
-          }]
-      }
-      });
-      // set chart options
-      setChartOptions(() => {
-        return {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
+            },{
+              label: 'Estimated severe Impact',
+              data: [
+                Math.trunc(data.severeImpact.currentlyInfected),
+                Math.trunc(data.severeImpact.infectionsByRequestedTime),
+                Math.trunc(data.severeImpact.severeCasesByRequestedTime),
+                Math.trunc(data.severeImpact.hospitalBedsByRequestedTime),
+                Math.trunc(data.severeImpact.casesForICUByRequestedTime),
+                Math.trunc(data.severeImpact.casesForVentilatorsByRequestedTime),
+                Math.trunc(data.severeImpact.dollarsInFlight)
+              ],
+              type: 'bar',
+              backgroundColor: '#dc3545',
+              borderColor: 
+                'red',
+              borderWidth: 1
+            }]
           }
-      }
+        });
+        // set chart options
+        setChartOptions(() => {
+          return {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
+          }
+        });
+        // set data.fetching to false
+        dispatch({ type: 'FETCHING', fetching: false });
+        if (data.impact.currentlyInfected){
+          // make chart visible in lg devices
+          document.getElementById('outputChart').classList.add('d-md-block');
+          document.getElementById('outputChart').focus();          
+          // make table visible in sm-devices 
+          document.getElementById('sm-output').classList.remove('d-none');
+          document.getElementById('sm-output').focus();
+        }
+
+      }).catch(error => {
+        dispatch({ type: 'FETCHING', fetching: false });
+        return error;
       });
-      // set data.fetching to false
-      dispatch({type: 'FETCHING', fetching: false});
-      console.log(`data ${data}`)
-    }).catch(error => {
-      dispatch({type: 'FETCHING', fetching: false});
-      return error;
-    });
-  }, [data, dispatch, setChartData, setChartOptions]);
+  }, [inputData, dispatch, setChartData, setChartOptions]);
+
 }
